faddba586a5682ae4dcb68f5455993277033398c
samSterns <sam.g.sterns@gmail.com>
2019-10-02 12:08:24 -0700

    game works and modest style

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-const correctNumber= 17
+import compareNumbers from './compare-numbers.js'
 
-const userNumber = document.getElementById('remaining-guesses');
+const correctNumber = 5;
 
-function compareNumbers(userNumber, correctNumber) => {
-    if (userNumber === correctNumber) return 0;
-    else if (userNumber > correctNumber) return 1;
-    else if (userNumber < correctNumber) return -1;
-};
-export default compareNumbers
+const userNumber = document.getElementById('user-number');
+
+const guessButton = document.getElementById('guess-button');
+
+const attemptsRemaining = document.getElementById('remaining-guesses');
+const popUpString = document.getElementById('pop-up-string');
+
+let attempts = 4;
+
+guessButton.addEventListener('click', () => {
+
+    attempts = attempts - 1;
+    console.log(attempts);
+    let userGuess = userNumber.value;
+    console.log(userGuess);
+
+    attemptsRemaining.textContent = attempts;
+    console.log(compareNumbers(userGuess, correctNumber));
+
+    userGuess = Number(userGuess);    
+
+    let result = compareNumbers(userGuess, correctNumber);
+
+    if (result === 0) {
+        popUpString.textContent = 'You Win!';
+        guessButton.disabled = true;
+    } else if (attempts === 0) {
+        guessButton.disabled = true;
+        popUpString.textContent = 'Out of Turns';
+    } else if (result === 1) {
+        popUpString.textContent = 'Guess is too high';
+    } else if (result === -1) {
+        popUpString.textContent = 'Guees is too low';
+    } 
+});
+// 
+
+//const compareNumbers = (attempts) => (
+   // if (a
+
